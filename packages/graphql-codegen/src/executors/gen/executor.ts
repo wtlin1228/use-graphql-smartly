983edb7138b3f652314725ae98b8f8e801c34eef
wtlin1228 <wtlin1228@gmail.com>
2023-08-03 19:56:53 +0800

    feat: use provided config file

--- packages/graphql-codegen/src/executors/gen/executor.ts
+++ packages/graphql-codegen/src/executors/gen/executor.ts
@@ -8,8 +8,9 @@ export default async function genExecutor(
   options: GenExecutorSchema,
   context: ExecutorContext
 ): Promise<{ success: boolean }> {
+  const configFile = options.configFile || 'codegen.ts';
   const { stdout, stderr } = await promisify(exec)(
-    `npx graphql-code-generator --config packages/${context.projectName}/codegen.ts`
+    `npx graphql-code-generator --config packages/${context.projectName}/${configFile}`
   );
 
   console.log(stdout);
